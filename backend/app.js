1128694e4db532338818458f5a47118ec54fb587
Micaela-Contreraz <contrerazmicaela@gmail.com>
2023-05-06 09:28:03 -0300

    Tarea Unidad 2: Peticiones, respuestas y templates

--- backend/app.js
+++ backend/app.js
@@ -1,57 +1,84 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+/** @format */
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var nosotrosRouter = require('./routes/nosotros');
-var novedadesRouter = require('./routes/novedades');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var loginRouter = require("./routes/admin/login");
+var session = require("express-session");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+app.use(
+  session({
+    secret: "hsdhuyrbfjkfkkdfsjad",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+/*
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/nosotros', nosotrosRouter);
-app.use('/novedades', novedadesRouter);
+*/
+app.get("/", function (req, res) {
+  var conocido = Boolean(req.session.nombre);
+  res.render("index", {
+    title: "Sesiones en Express.js",
+    conocido: conocido,
+    nombre: req.session.nombre,
+  });
+});
+app.post("/ingresar", function (req, res) {
+  if (req.body.nombre) {
+    req.session.nombre = req.body.nombre;
+  }
+  res.redirect("/");
+});
+app.get("/salir", function (req, res) {
+  req.session.destroy();
+  res.redirect("/");
+});
 
-app.get('/precios', (req, res, next) =>{
-  res.send('Hola soy la pagina de precios')
-})
-app.get('/usuarios', (req, res, next) =>{
-  res.send('Hola soy la pagina de usuarios')
-})
-app.get('/promociones', (req, res, next) =>{
-  res.send('Hola soy la pagina de promociones')
-})
+app.use("/admin/login", loginRouter);
 
+app.get("/precios", (req, res, next) => {
+  res.send("Hola soy la pagina de precios");
+});
+app.get("/usuarios", (req, res, next) => {
+  res.send("Hola soy la pagina de usuarios");
+});
+app.get("/promociones", (req, res, next) => {
+  res.send("Hola soy la pagina de promociones");
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
